f72f5a17e71857ea2f312d577fef384519e36dc2
PietraMassarotti <pietramassarotti@gmail.com>
2025-04-01 22:09:40 -0300

    Update and rename login to login.php

--- PHP/login.php
+++ PHP/login.php
@@ -36,7 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=], initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/form.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="CSS/form.css" media="screen" />
     <title>Sistema de sorteio</title>
 </head>
 
@@ -61,7 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <br>
         <br>
         <input type="submit" Value="Entrar" class="botao">
-        <a class="botao" href='/SistemaSorteio/index.php'>Criar nova conta</a>
+        <a class="botao" href='/PHP/index.php'>Criar nova conta</a>
     </form>
 </body>
 
